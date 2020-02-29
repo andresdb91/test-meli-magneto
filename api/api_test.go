862d939f36510de66d49cf8c082dc78deb0af6a1
Andres Bonilla <andresdb91@gmail.com>
2020-02-29 20:26:45 -0300

    Agrega test para GET /stats

--- api/api_test.go
+++ api/api_test.go
@@ -2,11 +2,15 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/andresdb91/test-meli-magneto/db/dbtest"
+	"github.com/andresdb91/test-meli-magneto/hll"
+	"github.com/andresdb91/test-meli-magneto/hll/hlltest"
 )
 
 func TestHttpPostMutant(t *testing.T) {
@@ -46,4 +50,57 @@ func TestHttpPostMutant(t *testing.T) {
 	dbtest.CleanupMockDb()
 }
 
-func TestHttpGetStats(t *testing.T) {}
+func TestHttpGetStats(t *testing.T) {
+	hll.Client = hlltest.SetupMockRedis()
+	countH, countM := hlltest.PopulateMockRedis(hll.Client)
+
+	expected := []struct {
+		key   string
+		value string
+	}{
+		{
+			key:   "count_human_dna",
+			value: fmt.Sprintf("%d", countH),
+		},
+		{
+			key:   "count_mutant_dna",
+			value: fmt.Sprintf("%d", countH),
+		},
+		{
+			key:   "ratio",
+			value: fmt.Sprintf("%2.1f", float32(countM/countH)),
+		},
+	}
+
+	router := setupRouter()
+
+	req, err := http.NewRequest("GET", "/stats", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusOK {
+		t.Errorf("getStats returned wrong code, got: %v, want: %v\n", status, http.StatusOK)
+	}
+
+	var res map[string]string
+	err = json.Unmarshal([]byte(rec.Body.String()), &res)
+
+	fmt.Println(res)
+
+	for _, c := range expected {
+		value, exists := res[c.key]
+		if exists {
+			if value != c.value {
+				t.Errorf("Incorrect value for key: %v, got: %v, want: %v\n", c.key, value, c.value)
+			}
+		} else {
+			t.Errorf("Expected key not found: %v\n", c.key)
+		}
+	}
+	hlltest.CleanupMockRedis(hll.Client)
+}
