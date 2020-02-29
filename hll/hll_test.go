bd13f89cd41088a24fed9c0f15a669189b5f60d5
Andres Bonilla <andresdb91@gmail.com>
2020-02-29 19:10:35 -0300

    Agrega test para agregar y contar con HLL

--- hll/hll_test.go
+++ hll/hll_test.go
@@ -1,11 +1,63 @@
 package hll
 
-import "testing"
+import (
+	"fmt"
+	"math"
+	"testing"
 
-func TestRedisSetup(t *testing.T) {
-	err := SetupHLL()
+	"github.com/go-redis/redis/v7"
+)
 
-	if err != nil {
-		t.Fatalf("Error al conectar a Redis: %s", err)
+var redisHLLTest = 2
+
+func TestHLLAddHuman(t *testing.T) {
+
+	Client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       redisHLLTest,
+	})
+
+	tolerance := 0.01
+
+	cases := []struct {
+		in    string
+		wantH int64
+		wantM int64
+	}{
+		{
+			in:    "humano_1",
+			wantH: 1,
+			wantM: 0,
+		},
+		{
+			in:    "humano_2",
+			wantH: 2,
+			wantM: 0,
+		},
+		{
+			in:    "humano_1",
+			wantH: 2,
+			wantM: 0,
+		},
+	}
+
+	for _, c := range cases {
+		var e float64
+		AddToHLL("human", c.in)
+
+		countH := GetCountHLL("human")
+		e = math.Abs(float64(countH-c.wantH)) / float64(c.wantH)
+		if e > tolerance {
+			t.Errorf("Returned human count doesn't match, got: %d, want: %d +/- %4.3f\n", countH, c.wantH, tolerance)
+		}
+
+		countM := GetCountHLL("mutant")
+		e = math.Abs(float64(countM-c.wantM)) / float64(c.wantM)
+		if e > tolerance {
+			t.Errorf("Returned mutant count doesn't match, got: %d, want: %d +/- %4.3f\n", countM, c.wantM, tolerance)
+		}
+
+		fmt.Printf("human: %d\nmutant: %d\n", countH, countM)
 	}
 }
